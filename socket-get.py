b32b4fec7061c8fe9b5ab452a1eff11db058982a
Kieran Harkin <k.harkin92@gmail.com>
2012-05-06 04:13:46 +0100

    POC test 2

--- socket-get.py
+++ socket-get.py
@@ -4,28 +4,32 @@
 #
 
 from socket import *
-import threading,sys,time,array
+import threading,sys,time,array,httplib
 
-BLOCK_SIZE = 128
+BLOCK_SIZE = 1024
 SPEED_UPDATE = 1
 DATA_RECV = 0
 DATA_LEN = 0
+THREADS_DONE = 0
 
 def buildRequest(host,adr,offset,end):
 	return "GET " + adr + " HTTP/1.1\r\nHost: " + host + "\r\nRange: bytes=" + str(offset) + "-" + str(end) + "\r\n\r\n\r\n"
 		
 class DownloadThread(threading.Thread):
-	def __init__(self, host, path, offset, adapter,fileHandle):
+	def __init__(self, host, path, offset, adapter,fileHandle,name):
 		threading.Thread.__init__(self)
+		self.name = name
 		self.file = open(fileHandle,'w')
 		self.file.seek(offset)
-		if offset != 0:
-			end = DATA_LEN/2
-		else:
+		
+		if offset>0:
 			end = DATA_LEN
+		else:
+			end = DATA_LEN/2
+			
 		try:
 			self.s = socket(AF_INET, SOCK_STREAM)
-			self.s.bind((adapter,1337))
+			self.s.bind((adapter,0))
 			self.s.connect( (host, 80) )
 			self.q = buildRequest(host,path,offset,end)
 			self.s.send(self.q)
@@ -33,18 +37,31 @@ class DownloadThread(threading.Thread):
 			print er
 
 	def run(self):
-		global DATA_RECV,DATA_LEN
+		global DATA_RECV,DATA_LEN,THREADS_DONE
 		headerClear = False
+		
+		self.data_len = 0
+		start = time.time()
+		resp = httplib.HTTPResponse(self.s)
+		resp.begin()
+		
 		while True:
 			try:
-				data = self.s.recv(BLOCK_SIZE)
-				self.file.write(data)
+				data = resp.read(BLOCK_SIZE)
+				if len(data)>0:
+					self.file.write(data)
+					self.data_len += len(data)
+				else:
+					THREADS_DONE += 1
+					print "\n" + self.name + ":~" + str(round(DATA_RECV/(time.time()-start)/1024,2)) + "kB/s"
+					return
 				DATA_RECV += len(data)
 			except Exception as er:
 				print er
 				return;
 		self.s.close()
 		print "Done"
+		
 class MainThread(threading.Thread):
 	def __init__(self,host,path):
 		threading.Thread.__init__(self)
@@ -58,7 +75,6 @@ class MainThread(threading.Thread):
 		q = buildRequest(host,path,0,'')
 		s.send(q)
 		d = s.recv(512)
-		print d
 		DATA_LEN = int(self.parseContentLength(d))
 		print "File size: " + self.humanize_bytes(DATA_LEN)
 		s.close()
@@ -69,17 +85,23 @@ class MainThread(threading.Thread):
 		f.truncate(DATA_LEN)
 		f.close()
 		
-		t = DownloadThread(host,path,0,"192.168.1.100",File)
-		t.start()
-		t2 = DownloadThread(host,path,DATA_LEN/2,"192.168.1.15",File)
-		t2.start()
+		self.t = DownloadThread(host,path,1,"192.168.1.100",File,"Thread-1")
+		self.t.start()
+		self.t2 = DownloadThread(host,path,DATA_LEN/2,"192.168.1.16",File,"Thread-2")
+		self.t2.start()
 		
 	def run(self):
+		global THREADS_DONE,DATA_RECV
 		downloading = True
-
+		start = time.time()
+		
 		while downloading:
 			sys.stdout.write("\r" + str(self.humanize_bytes(DATA_RECV)) + "   ")
 			sys.stdout.flush()
+			if THREADS_DONE==2:
+				print "\n~" + str(self.humanize_bytes(DATA_RECV/(time.time()-start))) + "/s"
+				print "Time taken: " + str(time.time()-start)
+				break
 			time.sleep(1)
 			
 	def parseContentLength(self,resp):
@@ -97,5 +119,5 @@ class MainThread(threading.Thread):
 			p += 1
 		return str(str(round(bytes,precision)) + " " + a[p])
 
-main = MainThread("minesrc.com","/imgs/screenshots/2012-02-03_18.08.48.png")
+main = MainThread("kharkin.ie","/SubExtract.zip")
 main.start()
